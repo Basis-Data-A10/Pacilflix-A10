1763b18e971f9a97ada8058369804aba2a45827f
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-04-27 18:29:20 +0700

    [FIX] Routing to login and register page success

--- authentication/views.py
+++ authentication/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
+from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
+from django.urls import reverse
 
 
 #@login_required(login_url='/login')
@@ -53,59 +55,33 @@ def show_landing(request):
 #     logout(request)
 #     return redirect('login')
 
-@csrf_exempt
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            auth_login(request, user)
-            # Status login sukses.
-            return JsonResponse({
-                "username": user.username,
-                "status": True,
-                "message": "Login sukses!"
-                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
-            }, status=200)
-        else:
-            return JsonResponse({
-                "status": False,
-                "message": "Login gagal, akun dinonaktifkan."
-            }, status=401)
-
-    else:
-        return JsonResponse({
-            "status": False,
-            "message": "Login gagal, periksa kembali username atau kata sandi."
-        }, status=401)
-    
-@csrf_exempt
-def logout(request):
-    username = request.user.username
-
-    try:
-        auth_logout(request)
-        return JsonResponse({
-            "username": username,
-            "status": True,
-            "message": "Logout berhasil!"
-        }, status=200)
-    except:
-        return JsonResponse({
-        "status": False,
-        "message": "Logout gagal."
-        }, status=401)
-
-@csrf_exempt
 def register(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
+    form = UserCreationForm()
 
-    if User.objects.filter(username=username).exists():
-        return JsonResponse({"status": False, "message": "Username already used."}, status=404)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('authentication:show_landing')
+    context = {'form':form}
+    return render(request, 'register_page.html', context)
 
-    user = User.objects.create_user(username=username, password=password)
-    user.save()
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("authentication:show_landing")) 
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login_page.html', context)
 
-    return JsonResponse({"username": user.username, "status": True, "message": "Register successful!"}, status=201)
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('authentication:show_landing'))
+    return response
