ba56bceac69287ae455aacb232c41f777d462a79
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-04-27 16:37:43 +0700

    [FIX] Trying to fix some issues

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from authentication.views import register, login_user, logout_user
+from authentication.views import register, login, logout, show_landing
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('register/', register, name='register'), 
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('', show_landing, name='show_landing'),
 ]
