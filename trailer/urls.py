7f764e46924659671c53d71bbf8f0663dd77af04
facesofgoblin <buatkuliahrana@gmail.com>
2024-05-02 22:14:23 +0700

    finishing

--- trailer/urls.py
+++ trailer/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='daftar_trailer.html'), name='daftar_trailer'),
-    path('hasil-search/', TemplateView.as_view(template_name='hasil_search.html'), name='hasil_search'),
+    path('hasil-search2/', TemplateView.as_view(template_name='hasil_search2.html'), name='hasil_search2'),
 
 ]
