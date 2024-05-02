d7d379ee8aca97aa4708381a6349d70e1930d441
facesofgoblin <buatkuliahrana@gmail.com>
2024-05-02 19:08:38 +0700

    delete unused views.py

--- ulasan/views.py
+++ ulasan/views.py
@@ -1,7 +1,2 @@
 from django.shortcuts import render
 
-
-def review_list(request):
-    # Generate range of numbers from 0 to 4
-    review_count = range(5)
-    return render(request, 'hal_ulasan.html', {'review_count': review_count})
