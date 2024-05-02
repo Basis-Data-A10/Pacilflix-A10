dd2c38fae544192771c583a0e5a50f41154563e0
facesofgoblin <buatkuliahrana@gmail.com>
2024-05-02 17:20:22 +0700

    Tayangan Selesai

--- tayangan/urls.py
+++ tayangan/urls.py
@@ -4,5 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='daftar_tayangan.html'), name='daftar_tayangan'),
+    path('hasil-search/', TemplateView.as_view(template_name='hasil_search.html'), name='hasil_search'),
+    path('hal-film/', TemplateView.as_view(template_name='hal_film.html'), name='hal_film'),
+    path('hal-series/', TemplateView.as_view(template_name='hal_series.html'), name='hal_series'),
+    path('hal-eps/', TemplateView.as_view(template_name='hal_eps.html'), name='hal_eps'),
     
 ]
