357004b39801b465a6a7f704f33c3e115bee8f11
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-05-10 11:15:03 +0700

    [REFACTOR] Edit urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
-from authentication.views import register, login_user, logout_user, show_landing
+from django.urls import path
+from authentication.views import show_register, show_login, show_landing
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('register/', register, name='register'), 
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path('register/', show_register, name='register'), 
+    path('login/', show_login, name='login'),
     path('', show_landing, name='show_landing'),
 ]
