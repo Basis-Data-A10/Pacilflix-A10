00ff113553415b82fccfc5a6343f4cf45b66899a
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-05-10 11:03:20 +0700

    [REFACTOR] Empty models.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-class Auth(models.Model):
-    username = models.TextField(null=True)
-    password = models.TextField(null=True)
-    negara_asal = models.CharField(max_length=255, null=True)
-
+# Create your models here.
