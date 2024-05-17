4123cf2828152374cbd1a1d1035eb9deff7087e3
ternaksapi <yusufhaikaln7@gmail.com>
2024-05-17 14:30:54 +0700

    [FEAT] RD For Daftar Favorite

--- daftar_favorit/urls.py
+++ daftar_favorit/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import show_favorite, show_favorite_detail, delete_favorite, delete_favorite_film
+
+app_name = 'daftar_favorit'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='daftar_favorit.html'), name='daftar_favorit'),
+    path('',show_favorite, name='daftar_favorit'),
+    path('delete/<str:favorite_timestamp>/', delete_favorite, name='delete_favorite'),
+    path('detail/<str:favorite_timestamp>/', show_favorite_detail, name='show_favorite_detail'),
+    path('delete_film/<str:favorite_timestamp>/<uuid:film_id>', delete_favorite_film, name='delete_favorite_film')
+
 ]
--- daftar_favorit/views.py
+++ daftar_favorit/views.py
@@ -2,6 +2,90 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from pacilflix_function.functions import *
+from django.db import connection
+from uuid import UUID
 
 def show_favorite(request):
-    return render(request, "daftar_favorit.html")
+    if not request.user.is_authenticated:
+        print("User is not authenticated")
+    
+    username = request.session.get('username')
+    print("Username from session: ", username)
+
+    if username is None:
+        print("Username is None")
+
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                       SELECT * FROM daftar_favorit
+                       WHERE username = %s
+                       """, [username])
+        
+        daftar_favorit = cursor.fetchall()
+
+        print("Daftar favorit: ", daftar_favorit)
+    
+    context = {
+        'daftar_favorit': daftar_favorit
+    }
+
+    return render(request, "daftar_favorit.html", context)
+
+def delete_favorite(request, favorite_timestamp):
+    if not request.user.is_authenticated:
+        print("User is not authenticated from delete_favorite")
+    
+    username = request.session.get('username')
+    
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                       DELETE FROM daftar_favorit
+                       WHERE timestamp = %s AND username = %s
+                       """, [favorite_timestamp, username])
+    
+    return HttpResponseRedirect(reverse('daftar_favorit:daftar_favorit'))
+
+def show_favorite_detail(request, favorite_timestamp):
+    if not request.user.is_authenticated:
+        print("User is not authenticated from show_favorite_detail")
+    
+    username = request.session.get('username')
+    
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                       SELECT t.judul, tf.timestamp, t.id
+                       FROM tayangan AS t
+                       INNER JOIN tayangan_memiliki_daftar_favorit AS tf
+                       ON t.id = tf.id_tayangan
+                       WHERE tf.timestamp = %s AND tf.username = %s
+                       """, [favorite_timestamp, username])
+        
+        favorite_detail = cursor.fetchall()
+
+        print("Favorite detail: ", favorite_detail)
+    
+    context = {
+        'favorite_detail': favorite_detail
+    }
+
+    return render(request, "daftar_favorit_detail.html", context)
+
+def delete_favorite_film(request, favorite_timestamp, film_id):
+    if not request.user.is_authenticated:
+        print("User is not authenticated from delete_favorite_film")
+    
+    username = request.session.get('username')
+    
+    try:
+        uuid_obj = UUID(str(film_id), version=4) # Ensure the ID is a valid UUID
+    except ValueError:
+        # Handle the error if film_id is not a valid UUID
+        return HttpResponseRedirect(reverse('daftar_favorit:daftar_favorit'))
+    
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                       DELETE FROM tayangan_memiliki_daftar_favorit
+                       WHERE timestamp = %s AND username = %s AND id_tayangan = %s
+                       """, [favorite_timestamp, username, str(uuid_obj)])
+    
+    return HttpResponseRedirect(reverse('daftar_favorit:daftar_favorit'))
