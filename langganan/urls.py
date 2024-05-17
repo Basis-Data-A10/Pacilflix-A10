ae771bc7a5272752f97ec491456dc19725eec3f3
arjunajaa <arju.naja@ui.ac.id>
2024-05-17 20:58:25 +0700

    fix bug

--- langganan/urls.py
+++ langganan/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from langganan import views
+
+app_name = "langganan"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='langganan.html'), name='langganan'),
-    path('pembayaran/', TemplateView.as_view(template_name='belipaket.html'), name='pembayaran'),
+    path('', views.langganan, name='langganan'),
+    path('beli_paket/', views.beli_paket, name='beli_paket'),
+    path('daftar_kontributor/', views.daftar_kontributor, name='daftar_kontributor')
 ]
     
