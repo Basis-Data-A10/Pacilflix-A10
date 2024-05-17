3f3b3be236529eed5d2c9f6d900004991f17e604
facesofgoblin <buatkuliahrana@gmail.com>
2024-05-17 16:48:20 +0700

    Tayangan dan Ulasan Hampir Selesai

--- trailer/views.py
+++ trailer/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+import random
+from urllib.parse import quote
+import urllib.parse
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from datetime import datetime
+from django.http import HttpResponse
 # Create your views here.
 
 def trailer(request):
@@ -7,3 +15,14 @@ def trailer(request):
 
 def trailer_guest(request):
     return render(request, 'hal_trailer_guest.html')
+
+def show_hasil_pencarian_trailer(request, value):
+    checked_value = check_string_valid(value)
+    tayangan = query_result(f"""SELECT judul, sinopsis_trailer, url_video_trailer, release_date_trailer
+                                FROM "TAYANGAN"
+                                WHERE judul ILIKE '%{checked_value}%';""")
+
+    context = {"tayangan": tayangan,
+               "searchvalue": value}
+
+    return render(request, 'hasil_pencarian_trailer.html', context)
