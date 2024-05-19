5cb52a74aa07288673fd64f590faf2e5f57aeadc
arjunajaa <arju.naja@ui.ac.id>
2024-05-19 16:02:35 +0700

    fix error from trigger and views.py

--- TK4_A_10_Trigger/TK4_A_10_Trigger_4.sql
+++ TK4_A_10_Trigger/TK4_A_10_Trigger_4.sql
@@ -1,33 +1,32 @@
-CREATE OR REPLACE FUNCTION manage_package_update_or_insert()
+CREATE OR REPLACE FUNCTION manage_package()
 RETURNS TRIGGER AS
 $$
 DECLARE
-	is_active_package BOOLEAN;
+    is_active_package BOOLEAN;
 BEGIN
-	SELECT EXISTS (
-		SELECT 1
-		FROM TRANSACTION AS T
-		JOIN PAKET AS P ON P.nama = T.nama_paket
-		JOIN DUKUNGAN_PERANGKAT AS D ON D.nama_paket = P.nama
-		WHERE T.nama_paket IS NOT NULL
-		  AND T.username = NEW.username
-		  AND CURRENT_DATE BETWEEN T.start_date_time AND T.end_date_time
-	) INTO is_active_package;
+    SELECT EXISTS (
+        SELECT 1
+        FROM TRANSACTION AS T
+        JOIN PAKET AS P ON P.nama = T.nama_paket
+        JOIN DUKUNGAN_PERANGKAT AS D ON D.nama_paket = P.nama
+        WHERE T.nama_paket IS NOT NULL
+          AND T.username = NEW.username
+          AND CURRENT_DATE BETWEEN T.start_date_time AND T.end_date_time
+    ) INTO is_active_package;
 
-	IF is_active_package THEN
-		UPDATE TRANSACTION
-		SET nama_paket = NEW.nama_paket,
-		    start_date_time = CURRENT_DATE,
-		    end_date_time = CURRENT_DATE + INTERVAL '1 month',
-		    metode_pembayaran = NEW.metode_pembayaran,
-		    timestamp_pembayaran = CURRENT_TIMESTAMP
-		WHERE nama_paket IS NOT NULL
-		  AND username = NEW.username
-		  AND CURRENT_DATE BETWEEN start_date_time AND end_date_time;
-		RETURN NULL;
-	END IF;
+    IF is_active_package THEN
+        DELETE FROM TRANSACTION
+        WHERE nama_paket IS NOT NULL
+          AND username = NEW.username
+          AND CURRENT_DATE BETWEEN start_date_time AND end_date_time;
 
-	RETURN NEW;
+        INSERT INTO TRANSACTION (username, start_date_time, end_date_time, nama_paket, metode_pembayaran, timestamp_pembayaran)
+        VALUES (NEW.username, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 month', NEW.nama_paket, NEW.metode_pembayaran, CURRENT_TIMESTAMP);
+
+        RETURN NULL;
+    END IF;
+
+    RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
